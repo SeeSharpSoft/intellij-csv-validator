778fdbff91ee334df3ae4aaf89c8958c02556992
Geek <github@seesharpsoft.net>
2017-09-26 21:54:24 +0200

    use PlainTextLanguage as BaseLanguage for CsvLanguage

--- src/main/java/net/seesharpsoft/intellij/plugins/csv/CsvLanguage.java
+++ src/main/java/net/seesharpsoft/intellij/plugins/csv/CsvLanguage.java
@@ -1,12 +1,13 @@
 package net.seesharpsoft.intellij.plugins.csv;
 
 import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.PlainTextLanguage;
 
 public class CsvLanguage extends Language {
     public static final CsvLanguage INSTANCE = new CsvLanguage();
 
     private CsvLanguage() {
-        super("csv");
+        super(PlainTextLanguage.INSTANCE, "csv");
     }
 
     @Override
