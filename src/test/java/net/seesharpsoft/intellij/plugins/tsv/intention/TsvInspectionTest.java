23c205237d7a44f71cdef3ee0c88a0ea95336980
GeeK <mail@seesharpsoft.net>
2018-05-20 16:19:17 +0200

    [FIX] fix "Add Separator" inspection for .tsv files

---
+++ src/test/java/net/seesharpsoft/intellij/plugins/tsv/intention/TsvInspectionTest.java
@@ -0,0 +1,45 @@
+package net.seesharpsoft.intellij.plugins.tsv.intention;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import net.seesharpsoft.intellij.plugins.csv.intention.CsvValidationInspection;
+
+public class TsvInspectionTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "./src/test/resources/inspection";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        myFixture.enableInspections(CsvValidationInspection.class);
+    }
+    
+    protected void doTestIntention(String testName, String hint) throws Throwable {
+        myFixture.configureByFile(testName + "/before.tsv");
+        final IntentionAction action = myFixture.filterAvailableIntentions(hint).stream()
+                .filter(intentionAction -> intentionAction.getText().equals(hint))
+                .findFirst().get();
+        
+        myFixture.launchAction(action);
+        myFixture.checkResultByFile(testName + "/after.tsv");
+    }
+    
+    public void testAddClosingQuote() throws Throwable {
+        doTestIntention("AddClosingQuote", "Add closing quote");
+    }
+
+    public void testAddMissingSeparator() throws Throwable {
+        doTestIntention("AddMissingSeparator", "Add separator");
+    }
+
+    public void testSurroundWithQuotes() throws Throwable {
+        doTestIntention("SurroundWithQuotes", "Surround with quotes");
+    }
+
+    public void testInvalidRange() throws Throwable {
+        doTestIntention("AddMissingSeparator", "Add separator");
+    }
+}
