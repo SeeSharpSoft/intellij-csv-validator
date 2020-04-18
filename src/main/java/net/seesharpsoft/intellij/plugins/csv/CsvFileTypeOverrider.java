c11b0d46134e1e83127e630a7cb61e5bc1c54446
GeeK <mail@seesharpsoft.net>
2020-04-18 20:04:44 +0200

    [FIX] ensure association of CSV/TSV/PSV files to this plugin

---
+++ src/main/java/net/seesharpsoft/intellij/plugins/csv/CsvFileTypeOverrider.java
@@ -0,0 +1,31 @@
+package net.seesharpsoft.intellij.plugins.csv;
+
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.impl.FileTypeOverrider;
+import com.intellij.openapi.vfs.VirtualFile;
+import net.seesharpsoft.intellij.plugins.psv.PsvFileType;
+import net.seesharpsoft.intellij.plugins.tsv.TsvFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CsvFileTypeOverrider implements FileTypeOverrider {
+    @Nullable
+    @Override
+    public FileType getOverriddenFileType(@NotNull VirtualFile file) {
+        if (file != null) {
+            String extension = file.getExtension();
+            if (extension != null) {
+                switch (extension.toLowerCase()) {
+                    case "csv":
+                        return CsvFileType.INSTANCE;
+                    case "tsv":
+                    case "tab":
+                        return TsvFileType.INSTANCE;
+                    case "psv":
+                        return PsvFileType.INSTANCE;
+                }
+            }
+        }
+        return null;
+    }
+}
