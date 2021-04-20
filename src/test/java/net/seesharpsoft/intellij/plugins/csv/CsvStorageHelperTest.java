4eccb2a9fcc33d0f7e7ba6f651f9f85518b4468c
GeeK <mail@seesharpsoft.net>
2021-04-20 10:48:54 +0200

    [TEST] system independent test

--- src/test/java/net/seesharpsoft/intellij/plugins/csv/CsvStorageHelperTest.java
+++ src/test/java/net/seesharpsoft/intellij/plugins/csv/CsvStorageHelperTest.java
@@ -12,35 +12,34 @@ public class CsvStorageHelperTest extends BasePlatformTestCase {
         super.setUp();
         Paths.get(this.getProject().getBasePath(), "csv_file_test.csv").toFile().createNewFile();
         Paths.get(this.getProject().getBasePath(), "test").toFile().mkdir();
-        Paths.get(this.getProject().getBasePath(), "test/py_file_test.py").toFile().createNewFile();
+        Paths.get(this.getProject().getBasePath(), "test", "py_file_test.py").toFile().createNewFile();
     }
 
     @Override
     protected void tearDown() throws Exception {
-        // TODO delete or not delete, that's the question
-        // Paths.get(this.getProject().getBasePath(), "csv_file_test.csv").toFile().delete();
-        // Paths.get(this.getProject().getBasePath(), "test/py_file_test.py").toFile().delete();
-        // Paths.get(this.getProject().getBasePath(), "test").toFile().delete();
+        Paths.get(this.getProject().getBasePath(), "csv_file_test.csv").toFile().delete();
+        Paths.get(this.getProject().getBasePath(), "test", "py_file_test.py").toFile().delete();
+        Paths.get(this.getProject().getBasePath(), "test").toFile().delete();
         super.tearDown();
     }
 
     public void testGetFileInProjectExists() {
-        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), "csv_file_test.csv");
+        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), Paths.get("/csv_file_test.csv").toString());
         assertNotNull(vf);
     }
 
     public void testGetFileInProjectDirectory() {
-        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), "\\test\\py_file_test.py");
+        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), Paths.get("/test/py_file_test.py").toString());
         assertNotNull(vf);
     }
 
     public void testGetFileInProjectDoesNotExist() {
-        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), "do_not_exist_csv_file_test.csv");
+        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), Paths.get("/not_existing_csv_file_test.csv").toString());
         assertNull(vf);
     }
 
     public void testGetFileInProjectDirectoryDoesNotExist() {
-        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), "\\test2\\py_file_test.py");
+        VirtualFile vf = CsvStorageHelper.getFileInProject(this.getProject(), Paths.get("/test2/py_file_test.py").toString());
         assertNull(vf);
     }
 }
