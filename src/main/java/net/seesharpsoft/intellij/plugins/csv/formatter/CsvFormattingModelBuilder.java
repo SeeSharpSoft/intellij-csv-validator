c67f4c85baa2ae2a330887f5f43676999f9980bb
GeeK <mail@seesharpsoft.net>
2022-10-24 19:59:09 +0200

    avoid unnecessary formatting while editing
    (huge performance boost)

--- src/main/java/net/seesharpsoft/intellij/plugins/csv/formatter/CsvFormattingModelBuilder.java
+++ src/main/java/net/seesharpsoft/intellij/plugins/csv/formatter/CsvFormattingModelBuilder.java
@@ -1,14 +1,12 @@
 package net.seesharpsoft.intellij.plugins.csv.formatter;
 
-import com.intellij.formatting.FormattingContext;
-import com.intellij.formatting.FormattingModel;
-import com.intellij.formatting.FormattingModelBuilder;
-import com.intellij.formatting.FormattingModelProvider;
+import com.intellij.formatting.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import net.seesharpsoft.UnhandledSwitchCaseException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,20 +15,29 @@ public class CsvFormattingModelBuilder implements FormattingModelBuilder {
     @Override
     @NotNull
     public FormattingModel createModel(FormattingContext formattingContext) {
-        PsiElement element = formattingContext.getPsiElement();
-        CodeStyleSettings settings = formattingContext.getCodeStyleSettings();
-        ASTNode root = CsvFormatHelper.getRoot(element.getNode());
-        CsvFormattingInfo formattingInfo = new CsvFormattingInfo(
-                settings,
-                CsvFormatHelper.createSpaceBuilder(settings),
-                CsvFormatHelper.createColumnInfoMap(root, settings)
-        );
+        switch (formattingContext.getFormattingMode()) {
+            case ADJUST_INDENT:
+            case ADJUST_INDENT_ON_ENTER:
+                // do not care about indent during editing (baaad performance)
+                return FormatterImpl.getInstance().createDummyFormattingModel(formattingContext.getPsiElement());
+            case REFORMAT:
+                PsiElement element = formattingContext.getPsiElement();
+                CodeStyleSettings settings = formattingContext.getCodeStyleSettings();
+                ASTNode root = CsvFormatHelper.getRoot(element.getNode());
+                CsvFormattingInfo formattingInfo = new CsvFormattingInfo(
+                        settings,
+                        CsvFormatHelper.createSpaceBuilder(settings),
+                        CsvFormatHelper.createColumnInfoMap(root, settings)
+                );
 
-        return FormattingModelProvider.createFormattingModelForPsiFile(
-                element.getContainingFile(),
-                new CsvBlock(root, formattingInfo),
-                settings
-        );
+                return FormattingModelProvider.createFormattingModelForPsiFile(
+                        element.getContainingFile(),
+                        new CsvBlock(root, formattingInfo),
+                        settings
+                );
+            default:
+                throw new UnhandledSwitchCaseException(formattingContext.getFormattingMode());
+        }
     }
 
     @Nullable
