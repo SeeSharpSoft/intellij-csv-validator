293eca297e7bb516a8172e5bc6307e7afcc13157
GeeK <mail@seesharpsoft.net>
2023-05-11 09:11:52 -0400

    FIX: Comment handling breaking in table editor #451

---
+++ src/test/java/net/seesharpsoft/intellij/plugins/csv/editor/table/swing/CsvTableCommentsAtBeginningTest.java
@@ -0,0 +1,21 @@
+package net.seesharpsoft.intellij.plugins.csv.editor.table.swing;
+
+import javax.swing.table.TableModel;
+
+public class CsvTableCommentsAtBeginningTest extends CsvTableEditorSwingTestBase {
+    @Override
+    protected String getTestDataPath() {
+        return "./src/test/resources/editor/table/swing";
+    }
+
+    @Override
+    protected String getTestFile() { return "CommentsAtBeginning.csv"; }
+
+    public void testHeaderIsFirstNonCommentLine() {
+        TableModel tableModel = fileEditor.getTable().getModel();
+
+        assertEquals("Col1 (1)", tableModel.getColumnName(0));
+        assertEquals("Col2 (2)", tableModel.getColumnName(1));
+        assertEquals("Col3 (3)", tableModel.getColumnName(2));
+    }
+}
